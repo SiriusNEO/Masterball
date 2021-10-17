dc30ee82528a1419468f2e707e8aca08d6604897
SiriusNEO <1713833595@qq.com>
2021-10-17 22:47:44 +0800

    semantic judge trial

--- build.bash
+++ build.bash
@@ -1,4 +1,4 @@
 set ff=UNIX
 set -e
 mkdir -p "out"
-find ./src -name "*.java"|javac -d out -cp /ulib/java/antlr-4.7.2-complete.jar:. @/dev/stdin
+find ./src -name "*.java"|javac -d out -cp ./lib/java/antlr-4.7.2-complete.jar:. @/dev/stdin
