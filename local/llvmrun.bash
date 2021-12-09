f7b443c9b4f29e702370dc43c775d594aae4eb65
SiriusNEO <1713833595@qq.com>
2021-12-09 21:12:28 +0800

    remove some files

--- local/llvmrun.bash
+++
@@ -1,6 +0,0 @@
-set -e
-chmod 777 Masterball.jar
-java -jar Masterball.jar -i test.mx -ast-o ast.out -llvm-o test.ll -debug-o log.txt
-cp ../lib/builtin.ll builtin.ll
-clang test.ll builtin.ll -o code
-./code
